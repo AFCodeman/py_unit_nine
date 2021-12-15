ce21ec6c94f9c38933f33e5bbf320a0241897aa0
anthonyfagiolo <fagioan@ssfs.org>
2021-12-15 12:30:22 -0500

    started on the assignment nine project

---
+++ assignment_nine.py
@@ -0,0 +1,22 @@
+import card
+import deck
+
+class War:
+    def __init__(self):
+        pass
+    def deal_cards(self, Deck):
+        cards = []
+        for x in range(5):
+            list.append (self.Card)
+        return cards
+    def compare_cards(self, card1, card2):
+        for x in range(5):
+            self.card1 = deal_cards()
+            self.card2 = deal_cards()
+            if __gt__(self.card1 > self.card2):
+                return "Player wins"
+            if __gt__(self.card2 > self.card1):
+                return "Computer wins"
+
+def main():
+    pass
