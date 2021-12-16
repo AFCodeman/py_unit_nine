2984ed74316432d1e2d050112be03fe09564a0a9
anthonyfagiolo <fagioan@ssfs.org>
2021-12-16 11:00:20 -0500

    worked on assignment 9

--- assignment_nine.py
+++ assignment_nine.py
@@ -1,22 +1,25 @@
 import card
 import deck
 
-class War:
-    def __init__(self):
-        pass
-    def deal_cards(self, Deck):
-        cards = []
-        for x in range(5):
-            list.append (self.Card)
-        return cards
-    def compare_cards(self, card1, card2):
-        for x in range(5):
-            self.card1 = deal_cards()
-            self.card2 = deal_cards()
-            if __gt__(self.card1 > self.card2):
-                return "Player wins"
-            if __gt__(self.card2 > self.card1):
-                return "Computer wins"
-
-def main():
-    pass
+def deal_cards(new_deck):
+    cards = []
+    for x in range(5):
+        cards.append(new_deck.deal())
+    return cards
+def compare_cards(card1, card2):
+    winner = 0
+    loser = 0
+    for x in range(5):
+        if card1>card2
+            print("Player wins")
+            winner += 1
+        elif __gt__(self.card2 > self.card1):
+            print("Computer wins")
+            loser += 1
+    if winner > loser:
+        return("Player wins by ", winner - loser)
+    else:
+        return ("Computer wins by ", loser - winner)
+def main(self):
+    self.deal_cards()
+    self.compare_cards()
--- testing_cards.py
+++ testing_cards.py
@@ -1,5 +1,6 @@
 import card
 import deck
+import assignment_nine
 
 #new_card = card.Card(9,"Hearts")
 #print(new_card.suit)
@@ -7,5 +8,7 @@ import deck
 #other_card = card.Card(5, "Clubs")
 #print(other_card.rank)
 
-new_deck = deck.Deck()
-print(new_deck.cards[-1].rank + " of " + new_deck.cards[-1].suit)
+#new_deck = deck.Deck()
+#print(new_deck.cards[-1].rank + " of " + new_deck.cards[-1].suit)
+
+print(assignment_nine.War())
